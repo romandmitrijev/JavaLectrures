9219b49cba0365aa434a716c9059dffe47422bf9
Roman Dmitrijev <roman@dmitrijev.net>
2019-01-16 16:05:00 +0200

    lecture seventeen commit

---
+++ src/com/lecture/seventeen/StackExample.java
@@ -0,0 +1,25 @@
+package com.lecture.seventeen;
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<String> stackOfCards = new Stack<>();
+        stackOfCards.push("Jack");
+        stackOfCards.push("Queen");
+        stackOfCards.push("King");
+        stackOfCards.push("Ace");
+
+        System.out.println(stackOfCards.toString());
+
+        String theLastInfo = stackOfCards.pop();
+        System.out.println(theLastInfo);
+
+        String cardAtTop = stackOfCards.peek();
+        System.out.println(cardAtTop);
+        System.out.println(stackOfCards);
+
+
+    }
+
+}
